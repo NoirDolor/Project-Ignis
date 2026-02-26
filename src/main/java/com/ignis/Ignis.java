e225cb2ae25ec22144242241680845b5e6762d53
Shivansh Kushwaha <isroshivansh@gmail.com>
2026-02-26 21:11:09 +0530

    Starting Point

---
+++ src/main/java/com/ignis/Ignis.java
@@ -0,0 +1,12 @@
+package com.ignis;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(Ignis.MODID)
+public class Ignis {
+    public static final String MODID = "ignis";
+
+    public Ignis() {
+        System.out.println("Ignis initialized.");
+    }
+}
